b7659205463fb46bbdc14455e59992019381de5d
Michał <michal.b2000@onet.pl>
2022-02-17 18:29:01 +0100

    Fix persistent config reading on cli target

--- targets/cli/FileScriptLoader.cpp
+++ targets/cli/FileScriptLoader.cpp
@@ -5,17 +5,16 @@ FileScriptLoader::FileScriptLoader() {
 
 void FileScriptLoader::loadScript(std::string scriptName, std::shared_ptr<berry::VmState> berry) {
     BELL_LOG(info, "fileloader", "Loading script: %s ", scriptName.c_str());
-    auto content = loadFile("../../../euphonium/scripts/" + scriptName);
+    auto content = loadFile(scriptName);
     if (!berry->execute_string(content)) {
         EUPH_LOG(error, "script_loader", "Failed to load script %s", scriptName.c_str());
     }
 }
 
 std::string FileScriptLoader::loadFile(std::string fileName) {
-    std::ifstream indexFile(fileName);
+    std::ifstream indexFile("../../../euphonium/scripts/" + fileName);
     std::string indexContent((std::istreambuf_iterator<char>(indexFile)),
             std::istreambuf_iterator<char>());
-
     return indexContent;
 }
 
